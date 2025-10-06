32707f11e160c45b09aacdd4ecb042e4ca0d3e7d
Moaz Jamil <jamilahmed20201970@gmail.com>
2025-10-06 17:15:07 +0500

    Portfolio

---
+++ script.js
@@ -0,0 +1,20 @@
+/* Typed.js Effect */
+var typed = new Typed(".typing", {
+  strings: ["Full Stack Developer", "Django & Flutter Expert", "POS System Builder"],
+  typeSpeed: 70,
+  backSpeed: 40,
+  loop: true
+});
+
+/* Particles.js Background */
+particlesJS("particles-js", {
+  "particles": {
+    "number": {"value": 70},
+    "color": {"value": "#0d6efd"},
+    "shape": {"type": "circle"},
+    "opacity": {"value": 0.5},
+    "size": {"value": 3},
+    "line_linked": {"enable": true, "color": "#0d6efd"},
+    "move": {"enable": true, "speed": 2}
+  }
+});
